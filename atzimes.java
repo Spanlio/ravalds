1e260509559cde74959a8978ae169ec2b5af5698
Spanlio <d.e.markovs@gmail.com>
2025-04-02 08:26:22 +0300

    save

--- atzimes.java
+++ atzimes.java
@@ -8,25 +8,28 @@ public class atzimes {
         Scanner sc  = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.#");
 
-    do { 
-            System.out.println("cik studentiem aprekinat gala vertejumu? : ");
-        while (!sc.hasNextInt()){
-            System.out.println("cik studentiem aprekinat gala vertejumu? : ");
-            sc.next();
-        }
-        studentuSk = sc.nextInt();
-    }while(studentuSk<1);
-    String[]studenti = new String[studentuSk];
+        do { 
+                System.out.println("cik studentiem aprekinat gala vertejumu? : ");
+            while (!sc.hasNextInt()){
+                System.out.println("cik studentiem aprekinat gala vertejumu? : ");
+                sc.next();
+            }
+            studentuSk = sc.nextInt();
+        }while(studentuSk<1);
+        String[]studenti = new String[studentuSk];
 
 
-    do{ 
-            System.out.println("kriteriju skaits : ");
-        while (!sc.hasNextInt()){
-            System.out.println("kriteriju skaits : ");
-            sc.next();
-        }
-        kriterijuSk = sc.nextInt();
-    }while(kriterijuSk<1);
-    String[]kriteriji = new String[kriterijuSk];
-}
+        do{ 
+                System.out.println("kriteriju skaits : ");
+            while (!sc.hasNextInt()){
+                System.out.println("kriteriju skaits : ");
+                sc.next();
+            }
+            kriterijuSk = sc.nextInt();
+        }while(kriterijuSk<1);
+        String[]kriteriji = new String[kriterijuSk];
+        int[]kriterijuSvars = new int[kriterijuSk];
+        int[][] kriterijuVertejums = new int[studentuSk][kriterijuSk];
+        double[] semVertejums = new double[studentuSk];
+    }
 }
