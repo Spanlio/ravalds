4ebb5370e62b501f696aa76a758daa07e1b7f011
Spanlio <d.e.markovs@gmail.com>
2025-04-02 08:23:31 +0300

    ievada kriteriju un studnetu skaitu

--- atzimes.java
+++ atzimes.java
@@ -1,71 +1,32 @@
-import java.util.*;
 
-class Kriterijs {
-    String nosaukums;
-    double svars; // procentos (piem., 50 nozīmē 50%)
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-    public Kriterijs(String nosaukums, double svars) {
-        this.nosaukums = nosaukums;
-        this.svars = svars / 100; // Pārvēršam decimāldaļās
-    }
-}
-
-class Student {
-    String vards;
-    Map<String, Integer> vertesanasKriteriji;
-
-    public Student(String vards) {
-        this.vards = vards;
-        this.vertesanasKriteriji = new HashMap<>();
-    }
-
-    public void pievienotVertejumu(String kriterijs, int vertejums) {
-        vertesanasKriteriji.put(kriterijs, vertejums);
-    }
-}
-
-class GradeCalculator {
-    List<Kriterijs> kriteriji;
-    
-    public GradeCalculator(List<Kriterijs> kriteriji) {
-        this.kriteriji = kriteriji;
-    }
-
-    public double aprekinatGalaVertejumu(Student student) {
-        double galaVertejums = 0;
-        for (Kriterijs k : kriteriji) {
-            Integer vertejums = student.vertesanasKriteriji.get(k.nosaukums);
-            if (vertejums != null) {
-                galaVertejums += vertejums * k.svars;
-            }
-        }
-        return Math.round(galaVertejums * 100.0) / 100.0; // Noapaļošana līdz 2 cipariem
-    }
-}
-
-public class StudentuVertejums {
+public class atzimes {
     public static void main(String[] args) {
-        // Definējam vērtēšanas kritērijus
-        List<Kriterijs> kriteriji = Arrays.asList(
-            new Kriterijs("Eksāmens", 50),
-            new Kriterijs("Mājasdarbi", 30),
-            new Kriterijs("Projekts", 20)
-        );
+        int studentuSk, kriterijuSk;
+        Scanner sc  = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.#");
 
-        // Izveidojam studentus un pievienojam vērtējumus
-        Student anna = new Student("Anna");
-        anna.pievienotVertejumu("Eksāmens", 8);
-        anna.pievienotVertejumu("Mājasdarbi", 9);
-        anna.pievienotVertejumu("Projekts", 10);
+    do { 
+            System.out.println("cik studentiem aprekinat gala vertejumu? : ");
+        while (!sc.hasNextInt()){
+            System.out.println("cik studentiem aprekinat gala vertejumu? : ");
+            sc.next();
+        }
+        studentuSk = sc.nextInt();
+    }while(studentuSk<1);
+    String[]studenti = new String[studentuSk];
 
-        Student janis = new Student("Jānis");
-        janis.pievienotVertejumu("Eksāmens", 7);
-        janis.pievienotVertejumu("Mājasdarbi", 6);
-        janis.pievienotVertejumu("Projekts", 9);
 
-        // Aprēķinam gala vērtējumu
-        GradeCalculator kalkulators = new GradeCalculator(kriteriji);
-        System.out.println("Anna - Gala vērtējums: " + kalkulators.aprekinatGalaVertejumu(anna));
-        System.out.println("Jānis - Gala vērtējums: " + kalkulators.aprekinatGalaVertejumu(janis));
-    }
+    do{ 
+            System.out.println("kriteriju skaits : ");
+        while (!sc.hasNextInt()){
+            System.out.println("kriteriju skaits : ");
+            sc.next();
+        }
+        kriterijuSk = sc.nextInt();
+    }while(kriterijuSk<1);
+    String[]kriteriji = new String[kriterijuSk];
+}
 }
